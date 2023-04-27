fd97cc1236af6a0dd571eed2cc972333ce31c2db
Jeanie <lumjeanie31@gmail.com>
2023-04-27 12:46:26 -0700

    remaining days

--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -16,4 +16,6 @@ int _putchar(int c);
 void print_times_table(int n);
 void positive_or_negative(int i);
 int largest_number(int a, int b, int c);
+void print_remaining_days(int month, int day, int year);
+int convert_day(int month, int day);
 #endif
