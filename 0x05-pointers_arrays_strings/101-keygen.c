f2e92984818665d71dae7cd033bcd2d47857e22f
Jeanie <lumjeanie31@gmail.com>
2023-05-02 14:06:14 -0700

    101-keygen.c

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -4,21 +4,19 @@
 
 /**
  * main - check the code
- *
  * Return: Always 0.
  */
 int main(void)
 {
-    char password[7];
-    int i;
+	char password[7];
+	int i;
 
-    srand(time(NULL));
+	srand(time(NULL));
 
-    for (i = 0; i < 7; i++) {
-        password[i] = rand() % 94 + 33;
-    }
+	for (i = 0; i < 7; i++)
+		password[i] = rand() % 94 + 33;
 
-    printf("%s\n", password);
+	printf("%s\n", password);
 
-    return (0);
+	return (0);
 }
